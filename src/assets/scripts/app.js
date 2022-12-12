316554b134503c1803e26297bec748acc1d2eec2
nanoshibayama <shibayama@nanomedia.jp>
2022-12-12 15:42:17 +0900

    コミットを作成

---
+++ src/assets/scripts/app.js
@@ -0,0 +1,16 @@
+"use strict";
+import './structure/adjustVh';
+import './structure/adjustViewport';
+// import Accordion from './components/accordion';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  // // アコーディオン
+  // const accordionEl = [...document.querySelectorAll('.js-accordion')].map(el => {
+  //   return new Accordion(el, {
+  //     tabs: '.js-accordion-tab',
+  //     panels: '.js-accordion-panel'
+  //   });
+  // });
+
+});
